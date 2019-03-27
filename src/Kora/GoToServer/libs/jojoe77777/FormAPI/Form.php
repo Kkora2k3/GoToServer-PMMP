83b90af7f11ad573bc7af3239955165e90c93c53
Kkora2k3 <43435939+Kkora2k3@users.noreply.github.com>
2019-03-27 18:07:22 +0700

    Update Form.php

--- src/Kora/GoToServer/libs/jojoe77777/FormAPI/Form.php
+++ src/Kora/GoToServer/libs/jojoe77777/FormAPI/Form.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Xenophilicy\NaviCompass\libs\jojoe77777\FormAPI;
+namespace Kora\GoToSevrer\libs\jojoe77777\FormAPI;
 
 use pocketmine\form\Form as IForm;
 use pocketmine\Player;
